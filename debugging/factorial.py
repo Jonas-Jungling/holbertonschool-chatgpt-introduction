31f6cef0a0e232293778c5c18934598db2caf94c
Jonas-Jungling <jonas.jungling@oclock.school>
2025-01-13 09:30:05 +0100

    Debugging python factorial

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+import sys
+
 def factorial(n):
     result = 1
     while n > 1:
@@ -5,5 +8,5 @@ def factorial(n):
         n -= 1  # Décrémenter n pour éviter la boucle infinie
     return result
 
-    f = factorial(int(sys.argv[1]))
-    print(f)
+f = factorial(int(sys.argv[1]))
+print(f)
